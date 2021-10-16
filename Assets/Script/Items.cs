e6ef34c757d6be90224eb9374aba82b3615524f9
Gragamella <57292660+Gragamella@users.noreply.github.com>
2021-10-16 17:40:43 +0200

    Files upload

---
+++ Assets/Script/Items.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items 
+{
+   public enum ItemType
+    {
+        Spada,
+        PozioneSalute,
+        PozioneVelocita,
+        PozioneSalto,
+        Lancia,
+        Frusta,
+        Monete,
+        Gioelli
+    }
+
+    public ItemType tipoOggetto;
+    public int quantità;
+    public string nome;
+
+    public Sprite GetSprite()
+    {
+        switch (tipoOggetto)
+        {
+            default:
+            case ItemType.Spada: return ItemsAssets.Instance.spriteSpada;
+            case ItemType.PozioneSalute: return ItemsAssets.Instance.spritePozioneSalute;
+            case ItemType.Monete: return ItemsAssets.Instance.spriteSoldi;
+            case ItemType.Lancia: return ItemsAssets.Instance.spriteLancia;
+            case ItemType.PozioneVelocita: return ItemsAssets.Instance.spritePozioneVelocita;
+            case ItemType.PozioneSalto: return ItemsAssets.Instance.spritePozioneSalto;
+        }
+    }
+
+    public string GetNomeOggetto()
+    {
+        switch (tipoOggetto)
+        {
+            default:
+            case ItemType.Spada: return ItemsAssets.Instance.spadaNome;
+            case ItemType.PozioneSalute: return ItemsAssets.Instance.pozioneSaluteNome;
+            case ItemType.Monete: return ItemsAssets.Instance.soldiNome;
+        }
+    }
+
+}
---
+++ Assets/Script/UI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public static UI instance;
+
+    public GameObject startMenu;
+    public InputField usernameField;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exist. Destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void ConnectToServer()
+    {
+        startMenu.SetActive(false);
+        usernameField.interactable = false;
+        Client.instance.ConnectToServer();
+    }
+
+}
